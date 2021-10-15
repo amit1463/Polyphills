77dfba917273ad958f1a853ad97ddd96cec30156
Amit Kumar Mandal <itsme.amit1230@gmail.com>
2021-10-15 23:15:47 +0530

    Update polyphills.js

--- polyphills.js
+++ polyphills.js
@@ -1,90 +1,54 @@
 // Polyphill for split
-
-const mySplit = (str,args) => {
-
-    let newStr = str.replaceAll(args,"*");
-
+let str = "Hello/-how/ar-e/you";
+String.prototype.mySplit = function(args){
+    let newStr = this.replaceAll(args,"*");
     // console.log(newStr);
-
     let arr = [];
-
     if(args === ""){
-
-      for(let char of str){
-
+      for(let char of this){
        arr.push(char)
-
       }
-
     }
-
     else if(newStr.includes("*")){
-
       let p1=0,p2;
-
       for(let i=0;i<newStr.length;i++){
-
         if(newStr.charAt(i) === "*"){
-
           p2=i;
-
           arr.push(newStr.slice(p1,p2));
-
           p1=p2+1;
-
         }
-
       }
-
       arr.push(newStr.slice(p2+1))
-
     }
-
     else{
-
-      arr.push(str);
-
+      arr.push(this);
     }
-
     return arr;
-
 }
 
-let str = "Hello/-how/ar-e/you"
-console.log("mySplit -- ",mySplit(str,"/")); // self made 
-console.log("Inbuilt split -- ",str.split("/")); // Inbuilt split
-
+console.log("mySplit -- ",str.mySplit("/")); // self made 
+console.log("Inbuilt split -- ",str.split("/")); // Inbuilt
 
 // polyphill for join
+let arr = [3,5,6,7,777,745645,474545];
 
-
-const newJoin = function (arr,sepr) {
-
+Array.prototype.newJoin = function(sepr) {
   let str = "";
-
   if(sepr === "") {
-    for(let i of arr){
+    for(let i of this){
         str+=i
     }
 
   }else if(sepr === undefined){
-    for(let i of arr){
-
+    for(let i of this){
         str+=i + ","
-
     }
-      
   }else{
-
-      for(let i of arr){
-
+      for(let i of this){
           str+=i + sepr
-
       }
-
   }
-
-   let a =  str.slice(0,str.indexOf(arr[arr.length - 1])) + arr[arr.length - 1]
+  let a =  str.slice(0,str.indexOf(this[this.length - 1])) + this[this.length - 1]
 
    if(sepr===""){
      return str;
@@ -94,24 +58,23 @@ const newJoin = function (arr,sepr) {
 
 }
 
-let arr = [2,4,5,253,2167,32]
-
-
-console.log("newJoin --- ",newJoin(arr,"-")) // self-made
-
+console.log("newJoin --- ",arr.newJoin("-")) // self-made
 console.log('Inbuilt Join --- ',arr.join("-")); // inbulit
 
 
+
+
+
 // polyphill for reverse
-function myReverse(arr){
+Array.prototype.newReverse=function(){
     let newArr =[];
-    for(let i=arr.length-1;i>=0;i--){
-      newArr.push(arr[i]);
+    for(let i=this.length-1;i>=0;i--){
+      newArr.push(this[i]);
     }
     return newArr;
   }
   
   let arr = [3,5,6,7,777,745645,474545];
-  console.log("newReverse -- ",myReverse(arr)) // self-made
+  console.log("newReverse -- ",arr.newReverse()) // self-made
   console.log("Inbuilt reverse -- ",arr.reverse()) // inbuilt
 
